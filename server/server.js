bdb618dd3d304cf9c0ad9b9ec4e48f4ed896550a
Alberto Carreras <a.carreras.c@gmail.com>
2018-10-13 18:24:46 -0400

    add tests to routes

--- server/server.js
+++ server/server.js
@@ -23,6 +23,45 @@ app.get('/todos', (req, res) => {
     })
 })
 
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (ObjectID.isValid(id)) {
+        Todo
+            .findById(id)
+            .then( (todo) => {
+                todo 
+                    ? res.send(todo)
+                    : res.status(404).send("No todo was found")
+            })
+            .catch( () => res.status(400).send("Couldn't process request"))
+    }
+    else {
+        res.status(404).send("Invalid ID")
+    }
+})
+
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo( {
+        text: req.body.text
+    })
+    todo.save().then((doc)=> {
+        res.send(doc);
+    }, (err)=> res.status(400).send(err))
+})
+
+app.listen(3000, () => {
+    console.log("Started on port 3000");
+});
+
+app.get('/users', (req, res) => {
+    User.find().then( (users) => {
+        res.send({users});
+    }, (err) => {
+        res.status(400).send(err);
+    })
+})
+
 app.get('/users/:id', (req, res) => {
     var id = req.params.id;
     if (ObjectID.isValid(id)) {
@@ -39,37 +78,5 @@ app.get('/users/:id', (req, res) => {
         res.status(404).send("Invalid ID")
     }
 })
-
-app.get('/todos/:id', (req, res) => {
-    var id = req.params.id;
-    if (ObjectID.isValid(id)) {
-        Todo
-            .findById(id)
-            .then( (todo) => {
-                user 
-                    ? res.send(todo)
-                    : res.status(404).send("No user was found")
-            })
-            .catch( () => res.status(400).send("Couldn't process request"))
-    }
-    else {
-        res.status(404).send("Invalid ID")
-    }
-})
-
-
-app.post('/todos', (req, res) => {
-    var todo = new Todo( {
-        text: req.body.text
-    })
-    todo.save().then((doc)=> {
-        res.send(doc);
-
-    }, (err)=> res.status(400).send(err))
-})
-
-app.listen(3000, () => {
-    console.log("Started on port 3000");
-});
 
 module.exports = {app};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,20 +1,46 @@
 // Loaded modules
 const expect = require('expect'); //https://github.com/mjackson/expect
 const request = require('supertest'); //https://github.com/visionmedia/supertest
+const {ObjectID} = require('mongodb');
 
 // Server
 const {app} = require('./../server')
 const {Todo} = require('./../models/todo')
+const {User} = require('./../models/user')
 
 
 //testing lifecycle method
-const todos = [{text: "First test to do"}, {text: "Second test to do"}];
+const todos = [{
+    text: "First test to do",
+    _id: new ObjectID()
+}, {
+    text: "Second test to do",
+    _id: new ObjectID()
+}];
+
+const users = [{
+    email: "acc@gmail.com",
+    _id: new ObjectID()
+}, {
+    email: "aln@gmail.com",
+    _id: new ObjectID()
+}];
+
 
 beforeEach( (done) => {
     Todo
         .remove({})
         .then( () => {
             return Todo.insertMany(todos)
+        })
+        .then( () => done() );
+});
+
+beforeEach( (done) => {
+    User
+        .remove({})
+        .then( () => {
+            return User.insertMany(users)
         })
         .then( () => done() );
 });
@@ -61,7 +87,7 @@ describe("POST /todos", () => {
                 if (err) {
                     return done(err);
                 }
-                
+
                 //Mongoose query
                 Todo
                 .find()
@@ -78,7 +104,6 @@ describe("POST /todos", () => {
 });
 
 describe("GET /todos", () => {
-
     it('should get all todos', (done) => {
         request(app)
             .get('/todos')
@@ -89,7 +114,84 @@ describe("GET /todos", () => {
             .end(done);
     } )
 
+    describe("GET /todos/:id", () => {
+        it('should return todo document', (done) => {
+            request(app)
+                .get(`/todos/${todos[0]._id.toHexString()}`)
+                .expect(200)
+                .expect( (res) => {
+                    expect(res.body.text).toBe(todos[0].text);
+                })
+                .end(done);
+        });
 
+        it('should return 404 Invalid ID', (done) => {
+            request(app)
+                .get(`/todos/${todos[0]._id.toHexString()}11`)
+                .expect(404)
+                .expect( (res) => {
+                    expect(res.text).toBe("Invalid ID")
+                })
 
+                .end(done);
+        });
 
+        it('should return 404 No todo was found', (done) => {
+            request(app)
+                .get(`/todos/${new ObjectID()}`)
+                .expect(404)
+                .expect( (res) => {
+                    expect(res.text).toBe("No todo was found")
+                })
+                .end(done);
+        });
+    });
 });
+
+describe("GET /users", () => {
+    // it('should get all users', (done) => {
+    //     request(app)
+    //         .get('/users')
+    //         .expect(200)
+    //         .expect( (res) => {
+    //             expect(res.body.users.length).toBe(2);
+    //         })
+    //         .end(done);
+    // } )
+
+    describe("GET /users/:id", () => {
+
+        it('should return user document', (done) => {
+            request(app)
+            .get(`/users/${users[0]._id.toHexString()}`)
+            .expect(200)
+            .expect( (res) => {
+                expect(res.body.email).toBe(users[0].email)
+            })
+            .end(done);
+        })
+
+        it('should return 404 Invalid ID', (done) => {
+            request(app)
+                .get(`/users/${todos[0]._id.toHexString()}11`)
+                .expect(404)
+                .expect( (res) => {
+                    expect(res.text).toBe("Invalid ID")
+                })
+
+                .end(done);
+        });
+
+        it('should return 404 No todo was found', (done) => {
+            request(app)
+                .get(`/users/${new ObjectID()}`)
+                .expect(404)
+                .expect( (res) => {
+                    expect(res.text).toBe("No user was found")
+                })
+                .end(done);
+        });
+
+    });
+});
+
